34f5e8df3879c5b6876c24e7bbd12d4ee70f7271
mshanemc <shane.mclaughlin@salesforce.com>
2019-05-01 16:31:53 -0500

    contact card example

---
+++ force-app/main/default/lwc/contactCard/contactCard.js
@@ -0,0 +1,17 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+
+const FIELDS = [
+    'Contact.Name',
+    'Contact.Title',
+    'Contact.Phone',
+    'Contact.Email',
+];
+
+export default class ContactCard extends LightningElement {
+    @api recordId;
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    contact;
+    
+}
