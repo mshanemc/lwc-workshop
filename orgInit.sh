2d6daff8486100824ac6f4328be6be7ec7ef53cb
mshanemc <shane.mclaughlin@salesforce.com>
2019-05-01 15:01:31 -0500

    proper tab name

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n LWC
-sfdx force:org:open -p /lightning/n/X1_Toast
+sfdx force:org:open -p /lightning/n/Toast
