8d9d16d679b5c86c0d512efccdb2ecac89bbce04
mshanemc <shane.mclaughlin@salesforce.com>
2019-05-09 09:10:38 -0500

    password generation

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n LWC
+sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/n/Toast
